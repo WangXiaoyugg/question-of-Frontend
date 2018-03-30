28a25734279a0c48d68c16b0457f3716a265e9ed
GarenWang <2270099149@qq.com>
2018-03-30 14:54:59 +0800

    weakmap

---
+++ demo/es2015/weakmap.js
@@ -0,0 +1,10 @@
+// weakMap 键会检查变量引用， 引用别删除，键值对
+const weakm = new WeakMap()
+
+let keyObject = {id: 1}
+const valObject = {score: 100}
+
+weakm.set(keyObject,valObject);
+weakm.get(keyObject) // => {score: 100}
+keyObject = null;
+console.log(weakm.has(keyObject))// false;
